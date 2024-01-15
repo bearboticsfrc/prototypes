a5d5828421b1ee4190d87e601cc40d468c14b878
Ben Griffin <Bennett.Griffin@gmail.com>
2024-01-15 12:35:56 -0700

    Updated shooter test for dynamic pid

--- Shooter test-Imported/src/main/java/frc/robot/Robot.java
+++ Shooter test-Imported/src/main/java/frc/robot/Robot.java
@@ -12,9 +12,11 @@ import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
 import edu.wpi.first.wpilibj.shuffleboard.ShuffleboardTab;
+import edu.wpi.first.wpilibj.shuffleboard.SimpleWidget;
 import edu.wpi.first.networktables.GenericEntry;
 
 import com.revrobotics.CANSparkFlex;
+import com.revrobotics.CANSparkMax;
 import com.revrobotics.RelativeEncoder;
 import com.revrobotics.SparkPIDController;
 import com.revrobotics.CANSparkBase;
@@ -33,16 +35,16 @@ public class Robot extends TimedRobot {
   private final ShuffleboardTab DRIVE_SHUFFLEBOARD_TAB = Shuffleboard.getTab("Drive");
   private final XboxController driverController = new XboxController(0);
 
-  private CANSparkFlex motor = new CANSparkFlex(17, MotorType.kBrushless);
-  private final CANSparkFlex motorFollower = new CANSparkFlex(16, MotorType.kBrushless);
+  private CANSparkMax motor = new CANSparkMax(4, MotorType.kBrushless);
+  private final CANSparkMax motorFollower = new CANSparkMax(14, MotorType.kBrushless);
   private RelativeEncoder motorEncoder;
   private SparkPIDController motorPidController;
 
-  private double p = 0.001;
-  private double i = 0.00000007; 
+  private double p = 0.0; // 0.001;
+  private double i = 0.00001; // 0.00000007; 
   private double d = 0.000000008;
-  private double iz = 400;
-  private double ff = 0.000168;
+  private double iz = 0; // 400;   2000?
+  private double ff = 0.0; // 0.000147;
   private double maxOutput = 1;
   private double minOutput = -1;
   private double targetRpm = 0;
@@ -64,11 +66,12 @@ public class Robot extends TimedRobot {
     checkRevError(motor.restoreFactoryDefaults());
     checkRevError(motorFollower.restoreFactoryDefaults());
     checkRevError(motor.enableVoltageCompensation(12.0));
+    motor.setInverted(false);
     checkRevError(motorFollower.enableVoltageCompensation(12.0));
     checkRevError(motor.setIdleMode(IdleMode.kCoast));
     checkRevError(motorFollower.setIdleMode(IdleMode.kCoast));
-    checkRevError(motorFollower.follow(motor, true));
-    checkRevError(motor.setClosedLoopRampRate(0.005));
+    checkRevError(motorFollower.follow(motor, false));
+    //checkRevError(motor.setClosedLoopRampRate(0.005));
 
     motorEncoder = motor.getEncoder();
   }
@@ -84,20 +87,44 @@ public class Robot extends TimedRobot {
     motorPidController.setOutputRange(minOutput, maxOutput);
   }
 
+  GenericEntry pEntry;
+  GenericEntry iEntry;
+  GenericEntry dEntry;
+  GenericEntry izEntry;
+  GenericEntry ffEntry;
+
+
   private void setupShuffleboardTab() {
-    DRIVE_SHUFFLEBOARD_TAB.add("P", p);
-    DRIVE_SHUFFLEBOARD_TAB.add("I", i);
-    DRIVE_SHUFFLEBOARD_TAB.add("D", d);
-    DRIVE_SHUFFLEBOARD_TAB.add("Iz", iz);
-    DRIVE_SHUFFLEBOARD_TAB.add("FF", ff);
+    pEntry = DRIVE_SHUFFLEBOARD_TAB.add("P", p).getEntry();
+    iEntry = DRIVE_SHUFFLEBOARD_TAB.add("I", i).getEntry();
+    dEntry = DRIVE_SHUFFLEBOARD_TAB.add("D", d).getEntry();
+    izEntry = DRIVE_SHUFFLEBOARD_TAB.add("Iz", iz).getEntry();
+    ffEntry = DRIVE_SHUFFLEBOARD_TAB.add("FF", ff).getEntry();
     DRIVE_SHUFFLEBOARD_TAB.add("minOutput", minOutput);
     DRIVE_SHUFFLEBOARD_TAB.add("maxOutput", maxOutput);
     DRIVE_SHUFFLEBOARD_TAB.addNumber("Timer", timer::get);
     DRIVE_SHUFFLEBOARD_TAB.addNumber("Shooter Motor Velocity", motorEncoder::getVelocity);
+    DRIVE_SHUFFLEBOARD_TAB.addNumber("Applied Output", motor::getAppliedOutput);
+    DRIVE_SHUFFLEBOARD_TAB.addNumber("Applied Current", motor::getOutputCurrent);
     DRIVE_SHUFFLEBOARD_TAB.addNumber("Filtered Velocity", () -> filteredVelocity);
     DRIVE_SHUFFLEBOARD_TAB.addNumber("Shooter Motor Output", motor::getAppliedOutput);
     DRIVE_SHUFFLEBOARD_TAB.addBoolean("AtVelocity?", this::atTargetVelocity);
   }
+
+   private void updateValues() {
+    p = pEntry.get().getDouble();
+    d = dEntry.get().getDouble();
+    i = iEntry.get().getDouble();
+    iz= izEntry.get().getDouble();
+    ff = ffEntry.get().getDouble();
+  }
+
+  @Override
+  public void teleopInit() {
+    updateValues();
+    setupPidController();
+  }
+
 
   @Override
   public void teleopPeriodic() {
@@ -105,6 +132,7 @@ public class Robot extends TimedRobot {
 
     if (driverController.getAButton() ) {
       if (timer.get() == 0) {
+        //checkRevError(motorPidController.setReference(targetRpm, CANSparkBase.ControlType.kVelocity));
         timer.start();
       }
     } else {
