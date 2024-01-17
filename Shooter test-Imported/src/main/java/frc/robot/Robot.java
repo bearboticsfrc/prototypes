7878e4f17e02cf73e8d7f928d3a89a3af4fb0b4b
Kellen <70085049+Kellenn@users.noreply.github.com>
2024-01-16 19:20:49 -0700

    Updates

--- Shooter test-Imported/src/main/java/frc/robot/Robot.java
+++ Shooter test-Imported/src/main/java/frc/robot/Robot.java
@@ -7,9 +7,8 @@
 
 package frc.robot;
 
-import edu.wpi.first.wpilibj.Timer;
+import edu.wpi.first.wpilibj.DataLogManager;
 import edu.wpi.first.wpilibj.TimedRobot;
-import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
 import edu.wpi.first.wpilibj.shuffleboard.ShuffleboardTab;
 import edu.wpi.first.networktables.GenericEntry;
@@ -22,28 +21,24 @@ import com.revrobotics.CANSparkBase.IdleMode;
 import com.revrobotics.CANSparkLowLevel.MotorType;
 import static frc.robot.util.RevUtil.checkRevError;
 
-
-import edu.wpi.first.math.filter.LinearFilter;
-
 /**
  * This is a demo program showing the use of the RobotDrive class, specifically
  * it contains the code necessary to operate a robot with tank drive.
  */
 public class Robot extends TimedRobot {
   private final ShuffleboardTab DRIVE_SHUFFLEBOARD_TAB = Shuffleboard.getTab("Drive");
-  private final XboxController driverController = new XboxController(0);
 
-  private final CANSparkFlex motor = new CANSparkFlex(4, MotorType.kBrushless);
-  private final CANSparkFlex motorFollower = new CANSparkFlex(14, MotorType.kBrushless);
+  private final CANSparkFlex motor = new CANSparkFlex(16, MotorType.kBrushless);
+  private final CANSparkFlex motorFollower = new CANSparkFlex(17, MotorType.kBrushless);
   private RelativeEncoder motorEncoder;
   private SparkPIDController motorPidController;
 
   private double targetRpm = 0;
-  private double p = 0.0; // 0.001;
-  private double i = 0.00001; // 0.00000007; 
-  private double d = 0.000000008;
+  private double p = 0.00028; // 0.001;
+  private double i = 0.000001; // 0.00000007; 
+  private double d = 0;
   private double iz = 0; // 400;   2000?
-  private double ff = 0.0; // 0.000147;
+  private double ff = 0; // 0.000147;
   private double maxOutput = 1;
   private double minOutput = -1;
 
@@ -53,11 +48,6 @@ public class Robot extends TimedRobot {
   private GenericEntry dEntry = DRIVE_SHUFFLEBOARD_TAB.add("d", d).getEntry();
   private GenericEntry izEntry = DRIVE_SHUFFLEBOARD_TAB.add("iz", iz).getEntry();
   private GenericEntry ffEntry = DRIVE_SHUFFLEBOARD_TAB.add("ff", ff).getEntry();
-
-  private Timer timer = new Timer();
-  private LinearFilter filter = LinearFilter.movingAverage(20);
-  private double filteredVelocity = 0;
-
 
   @Override
   public void robotInit() {
@@ -70,8 +60,10 @@ public class Robot extends TimedRobot {
     checkRevError(motor.restoreFactoryDefaults());
     checkRevError(motorFollower.restoreFactoryDefaults());
     checkRevError(motor.enableVoltageCompensation(12.0));
-    motor.setInverted(false);
+    motor.setInverted(true);
     checkRevError(motorFollower.enableVoltageCompensation(12.0));
+    checkRevError(motor.setSmartCurrentLimit(20));
+    checkRevError(motorFollower.setSmartCurrentLimit(20));
     checkRevError(motor.setIdleMode(IdleMode.kCoast));
     checkRevError(motorFollower.setIdleMode(IdleMode.kCoast));
     checkRevError(motorFollower.follow(motor, false));
@@ -94,13 +86,11 @@ public class Robot extends TimedRobot {
   private void setupShuffleboardTab() {
     DRIVE_SHUFFLEBOARD_TAB.add("minOutput", minOutput);
     DRIVE_SHUFFLEBOARD_TAB.add("maxOutput", maxOutput);
-    DRIVE_SHUFFLEBOARD_TAB.addNumber("Timer", timer::get);
     DRIVE_SHUFFLEBOARD_TAB.addNumber("Shooter Motor Velocity", motorEncoder::getVelocity);
     DRIVE_SHUFFLEBOARD_TAB.addNumber("Applied Output", motor::getAppliedOutput);
     DRIVE_SHUFFLEBOARD_TAB.addNumber("Applied Current", motor::getOutputCurrent);
-    DRIVE_SHUFFLEBOARD_TAB.addNumber("Filtered Velocity", () -> filteredVelocity);
     DRIVE_SHUFFLEBOARD_TAB.addNumber("Shooter Motor Output", motor::getAppliedOutput);
-    DRIVE_SHUFFLEBOARD_TAB.addBoolean("AtVelocity?", this::atTargetVelocity);
+    DRIVE_SHUFFLEBOARD_TAB.addNumber("Shooter Motor Temperature", motor::getMotorTemperature);
   }
 
    private void updateValues() {
@@ -115,6 +105,8 @@ public class Robot extends TimedRobot {
   public void teleopInit() {
     updateValues();
     setupPidController();
+    
+    DataLogManager.log("p = " + p + " i = " + i + " d = " + d);
   }
 
 
@@ -122,28 +114,11 @@ public class Robot extends TimedRobot {
   public void teleopPeriodic() {
     targetRpm = targetRpmEntry.getDouble(targetRpm);
 
-    if (driverController.getAButton() ) {
-      if (timer.get() == 0) {
-        timer.start();
-      }
-    } else {
-      timer.reset();
-    }
-
     if (targetRpm == 0) {
       motor.set(0);
     } else {
       checkRevError(motorPidController.setReference(targetRpm, CANSparkBase.ControlType.kVelocity));
     }
 
-    filteredVelocity = filter.calculate(motorEncoder.getVelocity());
-
-    if (atTargetVelocity()) {
-      timer.stop();
-    }
-  }
-
-  private boolean atTargetVelocity() {
-    return Math.abs(targetRpm - filteredVelocity) < 10;
   }
 }
